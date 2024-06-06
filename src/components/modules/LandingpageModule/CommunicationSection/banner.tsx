5a8aa39491e747c7f6e95ed44620c5a26374f3f2
hoangvh238 <hoangvh238.dev@gmail.com>
2024-06-06 20:17:57 +0700

    banner responsive

--- src/components/modules/LandingpageModule/CommunicationSection/banner.tsx
+++ src/components/modules/LandingpageModule/CommunicationSection/banner.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div className="mt-8 h-72 lg:mt-10">
+    <div className="mt-8 h-28 md:h-40 lg:h-52 xl:h-72 lg:mt-10">
       <Image
-        className="absolute left-0 w-full"
+        className="absolute left-0 w-full "
         src={CommunityBanner}
         alt={""}
       ></Image>
