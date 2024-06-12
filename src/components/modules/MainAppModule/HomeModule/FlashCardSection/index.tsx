809329f25d159714d7f80d5c7e915771e9c9c3cd
Kaede Developer <vuvo070403@gmail.com>
2024-06-12 21:33:02 +0700

    HomePage: Fix responsive

--- src/components/modules/MainAppModule/HomeModule/FlashCardSection/index.tsx
+++ src/components/modules/MainAppModule/HomeModule/FlashCardSection/index.tsx
@@ -16,34 +16,36 @@ function FlashCardSection() {
       </div>
       <div className="flex flex-col gap-5">
         <h4 className="font-bold">Đã xem gần đây</h4>
-        <div className=" grid grid-cols-1 lg:grid-cols-3  gap-4 lg:gap-[24px] xl:gap-[24px] lg:flex-row justify-between">
-          {recentData.map((item,index) => {
-            return <TitleCard flashCardData={item} key={index}/>;
+        <div className="grid grid-cols-1 justify-between gap-4 lg:grid-cols-3 lg:flex-row lg:gap-[24px] xl:gap-[24px]">
+          {recentData.map((item, index) => {
+            return <TitleCard flashCardData={item} key={index} />;
           })}
         </div>
       </div>
       <div className="flex flex-col gap-5">
         <h4 className="font-bold">Phổ biến</h4>
-        <Carousel
-          opts={{
-            align: "start",
-          }}
-          className="flex w-full flex-col gap-5 md:gap-[10px] select-none"
-        >
-          <CarouselContent className="h-fit">
-            {flashcardDatas.map((item, index) => (
-              <CarouselItem
-                key={index}
-                className='lg:basis-[49.7%] xl:basis-[33.3%] 2xl:basis-[33.3%]'
-              >
-                <div className="pb-1 px-1">
-                  <TitleCard flashCardData={item} key={index}/>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselDots/>
-        </Carousel>
+        <div>
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className="flex w-full select-none flex-col gap-5 md:gap-[10px]"
+          >
+            <CarouselContent className="h-fit">
+              {flashcardDatas.map((item, index) => (
+                <CarouselItem
+                  key={index}
+                  className="lg:basis-[49.7%] xl:basis-[33.3%] 2xl:basis-[33.3%]"
+                >
+                  <div className="px-1 pb-1">
+                    <TitleCard flashCardData={item} key={index} />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselDots />
+          </Carousel>
+        </div>
       </div>
     </div>
   );
