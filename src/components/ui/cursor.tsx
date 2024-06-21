0cb50f8959ec2128fa8b90bf26c1cfdf797d0a4d
Kaede Developer <vuvo070403@gmail.com>
2024-06-21 14:17:21 +0700

    Optimized

--- src/components/ui/cursor.tsx
+++ src/components/ui/cursor.tsx
@@ -44,7 +44,6 @@ function Cursor() {
           viewBox="0 0 40 40"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="-translate-x-4 -translate-y-4 scale-50"
         >
           <path
             fill="#444"
